dd32099f9de9994bc30001a6ad82fd553788079f
Isaac32614 <81194785+Isaac32614@users.noreply.github.com>
2021-04-26 19:43:14 -0300

    Update atividade12.py

--- atividade12.py
+++ atividade12.py
@@ -1,3 +1,4 @@
+from random import randint
 #1
 num1 = int(input("Digite um número: "))
 num2 = int(input("Digite um número: "))
@@ -36,4 +37,69 @@ for i in lista:
     if i%2 == 0:
         print(i)
         
- 
+ #7
+
+print(sum(lista))
+
+#8
+
+lista_str = '|'.join(map(str, lista))
+print(lista_str)
+
+#9
+lista.sort()
+print(lista)
+
+#10
+
+lista.sort(reverse = True)
+print(lista)
+
+#11
+
+lista2 = [4,5,2,0,9]
+print(lista2)
+
+#12
+
+lista = [3, 6, 3, 2, 1]
+
+b = lista[3:]
+
+print(b)
+
+#15
+
+lista10 = []
+c = 0
+while c < 11:
+    lista10.append(c)
+    c+=1
+
+print(lista10)
+
+#16
+
+lista20 = []
+d = 0
+while d <= 20:
+    if d%2 == 0:
+        lista20.append(d)
+        d+=1
+    else:
+        d+=1
+print(lista20)
+
+#17
+
+listaR = []
+print( [ randint(0,10) for i in range(20) ])
+
+#18
+listaMM = ["alo", "Alo", "aLO", "alO"]
+print( list( map( lambda x: x.upper(), listaMM ) ) )
+
+#19
+
+listaMN = ["alo ", " Alo ", " aLO ,"" al0"]
+print( list( map(lambda x: x.lower().strip, listaMN)))
